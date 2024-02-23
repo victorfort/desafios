0423743ce7f3527aa4d431c3846bab020e3b8aa1
victorfort <victor.and.fortunato@gmail.com>
2024-02-23 20:24:27 -0300

    cansei por hj

--- desafio03/script.js
+++ desafio03/script.js
@@ -1,46 +1,38 @@
-var v1 = "white";
-var v2 = "black";
-var v3 = "gray";
+
 var divFoto = document.querySelector("div#foto");
+var imgAtual = 1;
+var controle=1; //gambiarra pra controlar a parada das fotos ou passa creche ou passa banho
 
 function clicou(n){
     let desB = document.querySelector("div#banho")
     let desC = document.querySelector("div#creche")
-    if(n==1){
+    if(n==1 ){
         desB.style.display = "block";
         desC.style.display= "none";
-        setInterval(()=>floop(1), 2000);
+        controle=1;
+        setInterval(()=>floop(1, controle), 5000);
+        
         
     }else{
         desC.style.display = "block";
         desB.style.display= "none";
-        setInterval(()=>floop(2), 3000);
+        controle=2
+        setInterval(()=>floop(2, controle), 5000);
+        
 
     }
 }
-var imgAtual = 1;
-function floop(n){
+
+function floop(n,c2){
     
     const totalImages = 6; //baixei apenas 6 imagens
-    if(n==2){
-        imgAtual = (imgAtual % totalImages) +1;
+    if(n==2 && c2== 2){
+        imgAtual = (imgAtual % totalImages) +1; //quando a divisão resultar em zero, volta pra 1
         
         divFoto.style.backgroundImage = `url('imagens/creche/imagem0${imgAtual}.jpg')`;
-
+    }else{
+        imgAtual = (imgAtual % totalImages) +1; //quando a divisão resultar em zero, volta pra 1
         
-
+        divFoto.style.backgroundImage = `url('imagens/banho/banho0${imgAtual}.jpg')`;
     }
 }
-/*
-
-let currentImage = 1;
-const totalImages = 6;
-
-function changeImage() {
-    currentImage = (currentImage % totalImages) + 1;
-    document.getElementById("image").src = `imagem${currentImage}.jpg`;
-}
-
-setInterval(changeImage, 2000); // Alterna a imagem a cada 2 segundos (2000 milissegundos)
-
-*/
