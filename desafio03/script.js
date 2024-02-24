73623a59ebb35dcf0b7397463950846b3da19310
victorfort <victor.and.fortunato@gmail.com>
2024-02-23 21:14:18 -0300

    pronto

--- desafio03/script.js
+++ desafio03/script.js
@@ -1,7 +1,7 @@
 
 var divFoto = document.querySelector("div#foto");
 var imgAtual = 1;
-var controle=1; //gambiarra pra controlar a parada das fotos ou passa creche ou passa banho
+var controle; 
 
 function clicou(n){
     let desB = document.querySelector("div#banho")
@@ -9,15 +9,15 @@ function clicou(n){
     if(n==1 ){
         desB.style.display = "block";
         desC.style.display= "none";
-        controle=1;
-        setInterval(()=>floop(1, controle), 5000);
+        clearInterval(controle)
+        controle=  setInterval(()=>floop(1), 3000);
         
         
     }else{
         desC.style.display = "block";
         desB.style.display= "none";
-        controle=2
-        setInterval(()=>floop(2, controle), 5000);
+        clearInterval(controle)
+        controle = setInterval(()=>floop(2), 3000);
         
 
     }
@@ -26,7 +26,7 @@ function clicou(n){
 function floop(n,c2){
     
     const totalImages = 6; //baixei apenas 6 imagens
-    if(n==2 && c2== 2){
+    if(n==2 ){
         imgAtual = (imgAtual % totalImages) +1; //quando a divisão resultar em zero, volta pra 1
         
         divFoto.style.backgroundImage = `url('imagens/creche/imagem0${imgAtual}.jpg')`;
